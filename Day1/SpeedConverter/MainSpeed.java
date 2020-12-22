2b25c5566c863c8771d1a927d7b2baacbeeb598c
it2aswanishweta <52894895+it2aswanishweta@users.noreply.github.com>
2020-12-22 04:15:28 -0500

    Creating the main file for SpeedConverter

---
+++ Day1/SpeedConverter/MainSpeed.java
@@ -0,0 +1,29 @@
+package day1;
+
+import java.util.Scanner;
+
+public class MainSpeed {
+
+	public static void main(String[] args) {
+		
+		 Scanner sc = new Scanner(System.in);
+		 System.out.println("Kindly enter the speed");
+		 double speedKph = sc.nextDouble();
+				 
+		 SpeedConverter speedConverter = new SpeedConverter();
+		 long speedMph = speedConverter.accessSpeed(speedKph);
+		 
+	     if(speedMph != -1) {
+	    	 
+	    	 System.out.println("Value of Speed in Miles per hour is: "+ speedMph);;
+	     }
+	     else {
+	    	 
+	    	 System.out.println("Value of the speed entered should be greater than zero"); 	 
+	     }
+	     
+	     sc.close();
+
+	}
+
+}
